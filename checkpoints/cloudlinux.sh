056380eecdc4510dfec2989647733923244b2294
alihusain2 <ali.hussain@bab-albahrain.com>
2025-08-09 14:45:10 +0300

    refactor: remove CloudLinux activation check from script

--- checkpoints/cloudlinux.sh
+++ checkpoints/cloudlinux.sh
@@ -6,11 +6,3 @@ else
     echo "✘ CloudLinux is not installed."
     exit 1
 fi
-
-# Check if the CloudLinux is activated
-if [ -f /etc/cl_activation ]; then
-    echo "✔ CloudLinux is activated."
-else
-    echo "✘ CloudLinux is not activated."
-    exit 1
-fi
