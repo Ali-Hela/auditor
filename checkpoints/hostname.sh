8a315eed32670d72ae4c5469969e41094fe955fe
alihusain2 <ali.hussain@bab-albahrain.com>
2025-08-10 10:32:33 +0300

    refactor: update output symbol for FQDN compatibility check in hostname script

--- checkpoints/hostname.sh
+++ checkpoints/hostname.sh
@@ -4,7 +4,7 @@ if [ -z "$HOSTNAME" ]; then
     exit 1
 else
     if [[ ! "$HOSTNAME" =~ ^[a-zA-Z0-9]([-a-zA-Z0-9]*[a-zA-Z0-9])?(\.[a-zA-Z0-9]([-a-zA-Z0-9]*[a-zA-Z0-9])?)*$ ]]; then
-        echo "✘ Hostname is not FQDN compatible: $HOSTNAME"
+        echo "▲ Hostname is not FQDN compatible: $HOSTNAME"
         exit 1
     else
         echo "✔ Hostname is FQDN compatible: $HOSTNAME"
