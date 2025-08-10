7bdc149baca094b3214b6f43289aa38f0fc51314
alihusain2 <ali.hussain@bab-albahrain.com>
2025-08-10 13:36:47 +0300

    remove template

--- checkpoint-template.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Check if system timezone is Asia/Bahrain
-
-if timedatectl 2>/dev/null | grep -q 'Asia/Bahrain'; then
-    echo "✔ System timezone is set to Asia/Bahrain"
-else
-    echo "✘ System timezone is NOT set to Asia/Bahrain"
-fi
