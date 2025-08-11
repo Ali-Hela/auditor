4f61af30df2688b860abb9b7edc564f77b2a6006
alihusain2 <ali.hussain@bab-albahrain.com>
2025-08-11 15:14:26 +0300

    update config server scripts checkpoints

--- checkpoints/cse.sh
+++ checkpoints/cse.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Check if csf is installed
-if [ ! -f /etc/cse/cse.conf ]; then
+if [ -f /usr/local/cpanel/whostmgr/docroot/cgi/configserver/cse/cse.conf ]; then
+    echo "✔ CSE is installed."
+    exit 0
+elif [ -f /var/cpanel/apps/cse.conf ]; then
+    echo "✔ CSE is installed (legacy location)."
+    exit 0
+else
     echo "✘ CSE is not installed."
     exit 1
 fi
--- checkpoints/csf.sh
+++ checkpoints/csf.sh
@@ -5,12 +5,9 @@ if [ ! -f /etc/csf/csf.conf ]; then
     echo "✘ CSF is not installed."
     exit 1
 else
-    echo "✔ CSF is installed."
-fi
-
-# Check if CSF is out of testing mode
-if grep -q "TESTING = \"1\"" /etc/csf/csf.conf; then
-    echo "▲ CSF is in testing mode."
-else
-    echo "✔ CSF is not in testing mode."
+    if grep -q "TESTING = \"1\"" /etc/csf/csf.conf; then
+        echo "▲ CSF is installed but in testing mode."
+    else
+        echo "✔ CSF is installed."
+    fi
 fi
